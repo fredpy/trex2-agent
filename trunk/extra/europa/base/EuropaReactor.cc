0b5a2d26d3c21346ac542a8cda13ba18b05a54ac
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2011-05-22 10:02:23 +0000

    Added a very early version of EuropaReactor : 	- the only thing it does right now is load a model and create Internal and External timlenes on TREX accrodingly

---
+++ trunk/extra/europa/base/EuropaReactor.cc
@@ -0,0 +1,163 @@
+#include "EuropaReactor.hh"
+
+#include "PLASMA/PlanDatabase.hh"
+#include "PLASMA/Token.hh"
+
+using namespace TREX::europa;
+using namespace TREX::transaction;
+using namespace TREX::utils;
+
+/*
+ * class TREX::europa::EuropaReactor
+ */ 
+
+// structors 
+
+EuropaReactor::EuropaReactor(xml_arg_type arg)
+  :TeleoReactor(arg, false), m_assembly(*this) {
+  bool found;
+  std::string short_nddl = getName().str()+".nddl",
+    long_nddl = getGraphName().str()+short_nddl, 
+    nddl;
+  // locate nddl model 
+  //   - 1st look for <agent>.<reactor>.nddl
+  nddl = manager().use(long_nddl, found);
+  if( !found ) {
+    // if not found look for jsut <reactor>.nddl
+    nddl = manager().use(short_nddl, found);
+    if( !found )
+      // no model found for this reactor 
+      throw ReactorException(*this, "Unable to locate \""+long_nddl+"\" or \""+
+			     short_nddl+"\"");
+  }
+  // Load the model
+  if( !m_assembly.playTransaction(nddl) )
+    throw ReactorException(*this, "model in "+nddl+" is inconsistent.");
+  
+  if( !m_assembly.plan_db()->isClosed() ) {
+    syslog("WARN")<<"Plan Database is not closed:\n"
+		  <<"\tClosing it now!!\n";
+    m_assembly.plan_db()->close();
+  }
+
+  // Now load solver configuration 
+  std::string solver_cfg = parse_attr<std::string>(xml_factory::node(arg), 
+						   "solverConfig");  
+  if( solver_cfg.empty() )
+    throw XmlError(xml_factory::node(arg), "solverConfig attribute is empty");
+  // MISSING start : how to implement this ?
+
+  // MISSIN end
+  // finally I identify the external end internal timelines
+  std::list<EUROPA::ObjectId> objs;
+  m_assembly.trex_timelines(objs);
+  
+  for(std::list<EUROPA::ObjectId>::const_iterator it=objs.begin(); 
+      objs.end()!=it; ++it) {
+    EUROPA::LabelStr name = (*it)->getName();
+    EUROPA::LabelStr mode = m_assembly.mode(*it);
+    Symbol trex_name(name.c_str());
+    
+    if( Assembly::EXTERNAL_MODE==mode || Assembly::OBSERVE_MODE==mode ) 
+      use(trex_name, Assembly::OBSERVE_MODE!=mode);      
+    else if( Assembly::INTERNAL_MODE==mode ) {
+      provide(trex_name);
+      if( !isInternal(trex_name) ) {
+	// Formally it would be better to demote it as External
+	// but for now we will just hide this timeline to the rest of the world
+	syslog("WARNING")<<"Unable to declare "<<trex_name<<" as Internal ...\n"
+			 <<"\t making it Private.";
+      } 
+    } else if( Assembly::IGNORE_MODE==mode ) {
+      m_assembly.ignore(*it);
+    } 
+    // everything else is just private ...
+  }
+}
+
+EuropaReactor::~EuropaReactor() {}
+
+// callbacks
+
+//  - TREX transaction callback 
+
+void EuropaReactor::notify(Observation const &o) {
+}
+
+void EuropaReactor::handleRequest(goal_id const &g) {
+}
+
+void EuropaReactor::handleRecall(goal_id const &g) {
+  for(europa_mapping::iterator i=m_internal_goals.begin(); 
+      m_internal_goals.end()!=i; ++i)
+    if( i->second==g ) {
+      EUROPA::eint key = i->first;
+      m_internal_goals.erase(i);
+
+      // Need to notify the europa solver
+      // m_core->recall(key);
+      return;
+    }
+}
+
+//  - TREX execution callbacks
+
+void EuropaReactor::handleInit() {
+}
+
+void EuropaReactor::handleTickStart() {
+}
+
+bool EuropaReactor::synchronize() {
+}
+
+bool EuropaReactor::hasWork() {
+}
+
+void EuropaReactor::resume() {
+}
+
+//  - Europa interface callbacks
+
+void EuropaReactor::removed(EUROPA::TokenId const &tok) {
+  europa_mapping::iterator i = m_external_goals.find(tok->getKey());
+  if( m_external_goals.end()!=i ) 
+    m_external_goals.erase(i);
+  else {
+    i = m_internal_goals.find(tok->getKey());
+    m_internal_goals.erase(i);
+  }
+}
+
+void EuropaReactor::request(Symbol const &tl, 
+				EUROPA::TokenId const &tok) {
+  europa_mapping::iterator i = m_external_goals.find(tok->getKey());
+  if( m_external_goals.end()==i ) {
+    Goal myGoal(tl, tok->getUnqualifiedPredicateName().toString());
+    
+    // build the goal
+
+    goal_id request = postGoal(myGoal);
+    if( request )
+      m_external_goals[tok->getKey()] = request;
+  }  
+}
+
+void EuropaReactor::recall(EUROPA::TokenId const &tok) {
+  europa_mapping::iterator i = m_external_goals.find(tok->getKey());
+  if( m_external_goals.end()!=i ) {
+    goal_id g = i->second;
+    m_external_goals.erase(i);
+    postRecall(g);
+  }
+}
+
+void EuropaReactor::notify(Symbol const &tl, 
+			   EUROPA::TokenId const &tok) {
+  Observation obs(tl, tok->getUnqualifiedPredicateName().toString());
+  
+  // populate the observation 
+
+  postObservation(obs);
+}
+
---
+++ trunk/extra/europa/base/EuropaReactor.hh
@@ -0,0 +1,52 @@
+#ifndef H_EuropaReactor 
+# define H_EuropaReactor 
+
+# include "Assembly.hh"
+
+# include "TeleoReactor.hh"
+
+namespace TREX {
+  namespace europa {
+    
+    class EuropaReactor :public TREX::transaction::TeleoReactor {
+    public:
+      EuropaReactor(TREX::transaction::TeleoReactor::xml_arg_type arg);
+      ~EuropaReactor();
+
+      // EUROPA callbacks
+      void removed(EUROPA::TokenId const &tok);
+      void request(TREX::utils::Symbol const &tl, EUROPA::TokenId const &tok);
+      void recall(EUROPA::TokenId const &tok);
+      void notify(TREX::utils::Symbol const &tl, EUROPA::TokenId const &tok);
+
+      Assembly &assembly() {
+	return m_assembly;
+      }
+
+    private:
+      // TREX transaction callbacks
+      void notify(TREX::transaction::Observation const &o);
+      void handleRequest(TREX::transaction::goal_id const &g);
+      void handleRecall(TREX::transaction::goal_id const &g);
+
+      // TREX execution callbacks
+      void handleInit();
+      void handleTickStart();
+      bool synchronize();
+      bool hasWork();
+      void resume();
+      
+      typedef std::map<EUROPA::eint, TREX::transaction::goal_id> europa_mapping;
+
+      europa_mapping m_external_goals;
+      europa_mapping m_internal_goals;
+
+      Assembly m_assembly;
+      
+      friend class Assembly;
+    }; //TREX::europa::EuropaReactor
+
+  } // TREX::europa
+} // TREX
+
+#endif // H_EuropaReactor
