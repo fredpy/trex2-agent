bba59c0e616f717dc428e9eb7382f045c4a34863
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2011-06-11 00:23:12 +0000

    Made SingletonServer header private. This means that this header is not meant to be installed

--- trex/utils/SingletonDummy.cc
+++ trex/utils/SingletonDummy.cc
@@ -37,7 +37,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-#include "bits/SingletonServer.hh"
+#include "private/SingletonServer.hh"
 
 using namespace TREX::utils::internal;
 
