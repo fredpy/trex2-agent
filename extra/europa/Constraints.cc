26edba5ac229e21cdc6d7ebb27bf0dca5b95ff3a
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2011-06-12 22:13:05 +0000

    Added bind constraint

--- extra/europa/Constraints.cc
+++ extra/europa/Constraints.cc
@@ -33,7 +33,25 @@
  */
 #include "Constraints.hh"
 
-#include <PLASMA/Debug.hh>
+#include <PLASMA/RuleInstance.hh>
+#include <PLASMA/Token.hh>
+#include <PLASMA/TokenVariable.hh>
+
+EUROPA::TokenId TREX::europa::details::parent_token(EUROPA::ConstrainedVariableId const &var) {
+  EUROPA::EntityId e = var->parent();
+
+  if( e.isId() ) {
+    if( EUROPA::TokenId::convertable(e) )
+      return e;
+    
+    if( EUROPA::RuleInstanceId::convertable(e) ) {
+      EUROPA::RuleInstanceId r(e);
+      return r->getToken();
+    }
+  }
+  return EUROPA::TokenId::noId();
+}
+
 
 using namespace TREX::europa;
 
@@ -71,34 +89,50 @@ CheckExternal::CheckExternal(EUROPA::LabelStr const &name,
 			     EUROPA::LabelStr const &propagatorName,
 			     EUROPA::ConstraintEngineId const &cstrEngine,
 			     std::vector<EUROPA::ConstrainedVariableId> const &variables) 
-  :ReactorConstraint(name, propagatorName, cstrEngine, variables) {
-  if( m_variables.size()!=2 ) 
-    throw EuropaException("Constraint "+name.toString()+" require exaclty 2 arguments");
+  :ReactorConstraint(name, propagatorName, cstrEngine, variables), m_obj(NULL) {
+  if( m_variables.size()==2 ) {
+    m_obj = &(getCurrentDomain(m_variables[1]));
+  } else {
+    if( m_variables.size()!=1 )
+      throw EuropaException("Constraint "+name.toString()+" require 1 or 2 arguments");
+    EUROPA::TokenId tok = details::parent_token(m_variables[0]);
+    if( tok.isId() ) {
+      m_obj = &(getCurrentDomain(tok->getObject()));
+    }
+  }
   m_test = &(getCurrentDomain(m_variables[0]));
-  m_obj = &(getCurrentDomain(m_variables[1]));
-}
-
-bool CheckExternal::guardSatisfied() const {
-  return m_obj->isSingleton();
 }
 
 void CheckExternal::handleExecute() {
-  Assembly &r = assembly();
-  EUROPA::DataTypeId type(m_obj->getDataType());
-  EUROPA::LabelStr name(type->getName());
-  
-  if( r.schema()->isType(name) &&
-      r.schema()->isA(name, Assembly::TREX_TIMELINE) ) {
-    if( guardSatisfied() ) {
-      EUROPA::ObjectDomain const &dom=*m_obj;
-      EUROPA::ObjectId obj = dom.getObject(dom.getSingletonValue());
-      bool external = r.isExternal(obj);
+  if( NULL!=m_obj ) {
+    Assembly &r = assembly();
+    EUROPA::DataTypeId type = m_obj->getDataType();
 
-      m_test->intersect(external, external);
+    if( r.schema()->isA(type->getName(), Assembly::TREX_TIMELINE) ) {      
+      EUROPA::ObjectDomain const &dom(*m_obj);
+      std::list<EUROPA::ObjectId> exts, n_exts, vals = dom.makeObjectList();
+
+      for(std::list<EUROPA::ObjectId>::const_iterator i=vals.begin(); vals.end()!=i; ++i) {
+	if( r.isExternal(*i) )
+	  exts.push_back(*i);
+	else 
+	  n_exts.push_back(*i);
+      }
+      if( !exts.empty() ) {
+	if( n_exts.empty() ) {
+	  m_test->intersect(true, true);
+	} else if( m_test->isSingleton() ) {
+	  std::auto_ptr<EUROPA::ObjectDomain> tmp;
+	  if( m_test->isMember(true) )
+	    tmp.reset(new EUROPA::ObjectDomain(type, exts));
+	  else 
+	    tmp.reset(new EUROPA::ObjectDomain(type, n_exts));
+	  m_obj->intersect(*tmp);
+	}
+      }
     }
-  } else {
-    m_test->intersect(false, false);
   }
+  m_test->intersect(false, false);
 }
 
 /*
@@ -109,30 +143,86 @@ CheckInternal::CheckInternal(EUROPA::LabelStr const &name,
 			     EUROPA::ConstraintEngineId const &cstrEngine,
 			     std::vector<EUROPA::ConstrainedVariableId> const &variables) 
   :ReactorConstraint(name, propagatorName, cstrEngine, variables) {
-  if( m_variables.size()!=2 ) 
-    throw EuropaException("Constraint "+name.toString()+" require exaclty 2 arguments");
+  if( m_variables.size()==2 ) {
+    m_obj = &(getCurrentDomain(m_variables[1]));
+  } else {
+    if( m_variables.size()!=1 )
+      throw EuropaException("Constraint "+name.toString()+" require 1 or 2 arguments");
+    EUROPA::TokenId tok = details::parent_token(m_variables[0]);
+    if( tok.isId() ) {
+      m_obj = &(getCurrentDomain(tok->getObject()));
+    }
+  }
   m_test = &(getCurrentDomain(m_variables[0]));
-  m_obj = &(getCurrentDomain(m_variables[1]));
-}
-
-bool CheckInternal::guardSatisfied() const {
-  return m_obj->isSingleton();
 }
 
 void CheckInternal::handleExecute() {
-  Assembly &r = assembly();
-  EUROPA::DataTypeId type(m_obj->getDataType());
-  EUROPA::LabelStr name(type->getName());
-  
-  if( r.schema()->isType(name) &&
-      r.schema()->isA(name, Assembly::TREX_TIMELINE) ) {
-    if( guardSatisfied() ) {
-      EUROPA::ObjectDomain const &dom=*m_obj;
-      bool internal = r.isInternal(dom.getObject(dom.getSingletonValue()));
+  if( NULL!=m_obj ) {
+    Assembly &r = assembly();
+    EUROPA::DataTypeId type = m_obj->getDataType();
 
-      m_test->intersect(internal, internal);
+    if( r.schema()->isA(type->getName(), Assembly::TREX_TIMELINE) ) {      
+      EUROPA::ObjectDomain const &dom(*m_obj);
+      std::list<EUROPA::ObjectId> ints, n_ints, vals = dom.makeObjectList();
+
+      for(std::list<EUROPA::ObjectId>::const_iterator i=vals.begin(); vals.end()!=i; ++i) {
+	if( r.isInternal(*i) )
+	  ints.push_back(*i);
+	else 
+	  n_ints.push_back(*i);
+      }
+      if( !ints.empty() ) {
+	if( n_ints.empty() ) {
+	  m_test->intersect(true, true);
+	} else if( m_test->isSingleton() ) {
+	  std::auto_ptr<EUROPA::ObjectDomain> tmp;
+	  if( m_test->isMember(true) )
+	    tmp.reset(new EUROPA::ObjectDomain(type, ints));
+	  else 
+	    tmp.reset(new EUROPA::ObjectDomain(type, n_ints));
+	  m_obj->intersect(*tmp);
+	}
+      }
     }
-  } else 
-    m_test->intersect(false, false);
+  }
+  m_test->intersect(false, false);
 }
 
+/*
+ * class TREX::europa::Bind
+ */
+Bind::Bind(EUROPA::LabelStr const &name, 
+	   EUROPA::LabelStr const &propagatorName,
+	   EUROPA::ConstraintEngineId const &cstrEngine,
+	   std::vector<EUROPA::ConstrainedVariableId> const &variables)
+  :EUROPA::Constraint(name, propagatorName, cstrEngine, variables) {
+  if( m_variables.size()!=2 )
+    throw EuropaException("constraint "+name.toString()+" requires exactly 2 arguments");
+  m_target = &(getCurrentDomain(m_variables[0]));
+  m_default = &(getCurrentDomain(m_variables[1]));
+  // Check that types are compatibles
+  if( !m_target->getDataType()->isAssignableFrom(m_default->getDataType()) ) 
+    throw EuropaException("constraint "+name.toString()
+			  +" requires that the 2nd argument type is convertible to"
+			  " the first argement type");
+}
+
+bool Bind::guardSatisfied() const {
+  return m_default->isSingleton() && !m_target->isSingleton();
+}
+
+void Bind::handleExecute() {
+  if( guardSatisfied() ) {
+    if( m_target->intersects(*m_default) )
+      m_target->intersect(*m_default);
+    else {
+      EUROPA::edouble lb, ub, val = m_default->getSingletonValue();
+      m_target->getBounds(lb, ub);
+      EUROPA::edouble dlo = std::abs(lb-val), dhi = std::abs(ub-val);
+      if( dhi<dlo )
+	m_target->intersect(ub, ub);
+      else
+	m_target->intersect(lb, lb);
+    }
+  }
+}
