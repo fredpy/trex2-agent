1f22c42e256a2061d6f82472a69e9fa1429bf7a7
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-03-15 00:26:02 +0000

    - Added EarliestFirst flaw manager as a default extension - Adapted William code to work in the new trex

--- trunk/extra/starfish/Constraints.cc
+++ trunk/extra/starfish/Constraints.cc
@@ -38,6 +38,7 @@ namespace {
       TREX_REGISTER_CONSTRAINT(assembly,starfish::europa::CalcDistanceConstraint,
                                calcDistance, trex);
     
+      TREX_REGISTER_CONSTRAINT(assembly,starfish::europa::Bind,bind,Default);
       TREX_REGISTER_FLAW_MANAGER(assembly, starfish::europa::GoalManager_PR2,
                                  GoalManager);
       TREX_REGISTER_FLAW_FILTER(assembly, starfish::europa::GoalsOnlyFilter,
@@ -124,4 +125,51 @@ EUROPA::edouble CalcDistanceConstraint::compute(EUROPA::edouble x1, EUROPA::edou
 EUROPA::edouble CalcDistanceConstraint::compute(EUROPA::edouble a, EUROPA::edouble b){
 	EUROPA::edouble result = std::sqrt(std::pow(a, 2)+ std::pow(b, 2));
 	return result;
+}
+
+/*
+ * class starfish::europa::Bind
+ */
+Bind::Bind(EUROPA::LabelStr const &name, 
+	   EUROPA::LabelStr const &propagatorName,
+	   EUROPA::ConstraintEngineId const &cstrEngine,
+	   std::vector<EUROPA::ConstrainedVariableId> const &variables)
+  :EUROPA::Constraint(name, propagatorName, cstrEngine, variables) {
+  if( m_variables.size()!=2 )
+    throw TREX::europa::EuropaException("constraint "+name.toString()+" requires exactly 2 arguments");
+  m_target = m_variables[0];
+  m_default = m_variables[1];
+  // Check that types are compatibles
+  if( !m_target->getDataType()->isAssignableFrom(m_default->getDataType()) ) 
+    throw TREX::europa::EuropaException("constraint "+name.toString()
+			  +" requires that the 2nd argument type is convertible to"
+			  " the first argement type");
+}
+
+bool Bind::guardSatisfied() const {
+  return m_target->isActive() && m_default->lastDomain().isSingleton() && 
+    !m_target->lastDomain().isSingleton();
+}
+
+void Bind::handleExecute() {
+  if( guardSatisfied() ) {
+    EUROPA::Domain &domain = getCurrentDomain(m_target);
+    debugMsg("trex:bind", "BEFORE "<<m_target->getName().toString()
+	     <<"="<<m_target->toString()
+	     <<" "<<m_default->toString());
+    if( domain.intersects(m_default->lastDomain()) ) {
+      domain.intersect(m_default->lastDomain());
+    } else {
+      EUROPA::edouble lb, ub, val = m_default->lastDomain().getSingletonValue();
+      domain.getBounds(lb, ub);
+      EUROPA::edouble dlo = std::abs(lb-val), dhi = std::abs(ub-val);
+      if( dhi<dlo )
+	domain.intersect(ub, ub);
+      else
+	domain.intersect(lb, lb);
+    }
+    debugMsg("trex:bind", "AFTER "<<m_target->getName().toString()
+	     <<"="<<m_target->toString()
+	     <<" "<<m_default->toString());
+  }
 }
