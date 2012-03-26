e4b5b2d35d45179734b5cc994744f57c13a9d133
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-03-26 15:53:52 +0000

    [extensions]  - Added sqrt constraint

--- trunk/extra/europa/extensions/Numeric.cc
+++ trunk/extra/europa/extensions/Numeric.cc
@@ -34,9 +34,14 @@
 #include "Numeric.hh"
 
 #include <PLASMA/ConstrainedVariable.hh>
+#include <PLASMA/Domains.hh>
 
 
 using namespace TREX::europa;
+
+/*
+ * class TREX::europa::AbsValConstraint
+ */
 
 AbsValConstraint::AbsValConstraint(EUROPA::LabelStr const &name,
 				   EUROPA::LabelStr const &propagatorName,
@@ -78,4 +83,102 @@ void AbsValConstraint::handleExecute() {
     m_val.intersect(a_lb, a_ub);
   if( m_val.isMember(-a_lb) || m_val.isMember(-a_ub) )
     m_val.intersect(-a_ub, -a_lb);     
+}
+
+/*
+ * class TREX::europa::SqrtConstraint
+ */
+
+SqrtConstraint::SqrtConstraint(EUROPA::LabelStr const &name,
+                               EUROPA::LabelStr const &propagatorName,
+                               EUROPA::ConstraintEngineId const &cstrEngine,
+                               std::vector<EUROPA::ConstrainedVariableId> const &vars)
+:EUROPA::Constraint(name, propagatorName, cstrEngine, vars),
+m_sqrt(getCurrentDomain(vars[1])), 
+m_val(getCurrentDomain(vars[0])) {
+  checkError(vars.size()==2, "Exactly 2 parameters required.");
+}
+
+
+void SqrtConstraint::handleExecute() {
+  static EUROPA::IntervalDomain const positive(0.0, std::numeric_limits<EUROPA::edouble>::infinity());
+  static EUROPA::edouble const acceptable_epsilon(1e-4);
+
+  if( m_sqrt.isOpen() && m_val.isOpen() ) {
+    m_sqrt.intersect(positive);
+    m_val.intersect(positive);
+    return;
+  } else if( m_val.isSingleton() ) {
+    EUROPA::edouble val = m_val.getSingletonValue(), root;
+    
+    if( val<0.0 ) {
+      m_val.intersect(positive);
+      return;
+    }
+    root = std::sqrt(val);
+    if( val>=std::numeric_limits<EUROPA::edouble>::infinity() )
+      m_sqrt.intersect(root, std::numeric_limits<EUROPA::edouble>::infinity());
+    else 
+      m_sqrt.intersect(root, root);
+    return;
+  } else if( m_sqrt.isSingleton() ) {
+    EUROPA::edouble root = m_sqrt.getSingletonValue(), val;
+    
+    if( root<0.0 ) {
+      m_sqrt.intersect(positive);
+      return;
+    }
+    val = root*root;
+    if( val>=std::numeric_limits<EUROPA::edouble>::infinity() )
+      m_val.intersect(root, std::numeric_limits<EUROPA::edouble>::infinity());
+    else 
+      m_val.intersect(val, val);
+    return;
+  }
+  EUROPA::edouble v_min, v_max, r_min, r_max;
+  
+  for(bool done=false; !done; ) {
+    done = true;
+    m_val.getBounds(v_min, v_max);
+    m_sqrt.getBounds(r_min, r_max);
+    
+    if( v_min<=0.0 )
+      v_min = 0.0;
+    if( r_min<=0.0 )
+      r_min = 0.0;
+    
+    // compute upper bound of sqrt
+    EUROPA::edouble max_r, min_r;
+    if( v_max>=std::numeric_limits<EUROPA::edouble>::infinity() )
+      max_r = v_max;
+    else 
+      max_r = std::sqrt(v_max);
+    if( r_max-acceptable_epsilon > max_r )
+      r_max = max_r;
+    
+    // compute lower bound of sqrt
+    min_r = std::sqrt(v_min);
+    if( r_min+acceptable_epsilon < min_r )
+      r_min = min_r;
+    if( m_sqrt.intersect(r_min, r_max) && m_sqrt.isEmpty() )
+      return;
+    
+    EUROPA::edouble max_v, min_v;
+    
+    // compute upper bound of the square
+    max_v = r_max*r_max;
+    if( v_max-acceptable_epsilon > max_v )
+      v_max = max_v;
+    // compute the lower bound of the square
+    min_v = r_min*r_min;
+    if( v_min+acceptable_epsilon < min_v )
+      v_min = min_v;
+    
+    if( m_val.intersect(v_min, v_max) ) {
+      if( m_val.isEmpty() )
+        return;
+      // need to propagate the new domain to m_sqrt
+      done = false;
+    }
+  }
 }
