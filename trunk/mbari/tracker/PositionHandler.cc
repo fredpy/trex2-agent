75f3f0459e6a8cce94bcd3f634291cd1b9b90758
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-05-24 16:28:00 +0000

    [PositionHandler] Now we dynamically crate new timelines for all the assets tracked [dta.cfg] Defined a simple drifter tracking app to that posts drifter following goals to mbar dorado AUV through iridium

--- trunk/mbari/tracker/PositionHandler.cc
+++ trunk/mbari/tracker/PositionHandler.cc
@@ -15,69 +15,81 @@ namespace {
 }
 
 PositionHandler::PositionHandler(PositionHandler::xml_arg const &arg)
-  :MessageHandler(arg), 
-   m_asset(parse_attr<std::string>(factory::node(arg), "asset")) {
+:MessageHandler(arg) { 
   m_exchange += "_pb";
-
-  provide(m_asset);
-  notify(TREX::transaction::Observation(m_asset, "undefined"));
-  m_fresh = false;
 }
 
 bool PositionHandler::handleMessage(amqp::queue::message &msg) {
   MBARItracking::PlatformReport report;
   
   if( report.ParseFromArray(msg.body(), msg.size()) ) {
-    if( report.has_name() && m_asset==report.name() ) {
+    if( report.has_name() ) {
+      std::string asset = report.name();      
       time_t secs;
       double lat, lon;
       
-      // Check that it has the necessary attributes
-      if( report.has_epoch_seconds() ) {
-	secs = report.epoch_seconds();
-	if( report.has_latitude() ) {
-	  lat = report.latitude();
-	  if( report.has_longitude() ) {
-	    lon = report.longitude();
-	    double north, east;
-	    geo_to_utm(lat, lon, north, east);
-	    m_position.update(secs, north, east);
-	    m_fresh = true;
-	    return true;
-	  }
-	}
-      }
+      if( report.has_epoch_seconds() )
+        secs = report.epoch_seconds();
+      else
+        return false;
+      
+      if( report.has_latitude() )
+        lat = report.latitude();
+      else 
+        return false;
+      
+      if( report.has_longitude() )
+        lon = report.longitude();
+      else 
+        return false;
+      
+      asset_info base;
+      asset_map::iterator pos;
+      bool inserted;
+      double north, east;
+      
+      geo_to_utm(lat, lon, north, east);
+
+      boost::tie(pos, inserted) = m_assets.insert(asset_map::value_type(asset, base));
+      pos->second.second.update(secs, north, east);
+      pos->second.first = true;
+      if( inserted )
+        provide(asset);
+      return true;
     } 
   }
   return false;
 }
 
 bool PositionHandler::synchronize() {
-  if( m_fresh || m_position.have_speed() ) {
-    TREX::transaction::Observation obs(m_asset, "Holds");
-    time_t now_t = std::floor(tickToTime(now()));
-    long int dt;
-    point<2> vect(m_position.position(now_t, dt));
-    double lat, lon;
+  time_t now_t = std::floor(tickToTime(now()));
+  
+  for(asset_map::iterator i=m_assets.begin(); m_assets.end()!=i; ++i) {
+    if( i->second.first || i->second.second.have_speed() ) {
+      TREX::transaction::Observation obs(i->first, "Holds");
+      long int dt;
+      point<2> vect(i->second.second.position(now_t, dt));
+      double lat, lon;
     
-    m_fresh = false;
+      i->second.first = false;
 
-    double dtick = dt;
-    dtick /= tickDuration();
+      double dtick = dt;
+      dtick /= tickDuration();
     
-    obs.restrictAttribute("freshness", IntegerDomain(std::floor(dtick+0.5)));
+      obs.restrictAttribute("freshness", IntegerDomain(std::floor(dtick+0.5)));
 
-    obs.restrictAttribute("northing", FloatDomain(vect[0]));
-    obs.restrictAttribute("easting", FloatDomain(vect[1]));
-    utm_to_geo(vect[0], vect[1], lat, lon);
-    obs.restrictAttribute("latitude", FloatDomain(lat));
-    obs.restrictAttribute("longitude", FloatDomain(lon));
+      obs.restrictAttribute("northing", FloatDomain(vect[0]));
+      obs.restrictAttribute("easting", FloatDomain(vect[1]));
+      utm_to_geo(vect[0], vect[1], lat, lon);
+      obs.restrictAttribute("latitude", FloatDomain(lat));
+      obs.restrictAttribute("longitude", FloatDomain(lon));
     
-    if( m_position.have_speed() ) {
-      obs.restrictAttribute("speed_north", FloatDomain(m_position.speed()[0]));
-      obs.restrictAttribute("speed_east", FloatDomain(m_position.speed()[1]));
+      if( i->second.second.have_speed() ) {
+        obs.restrictAttribute("speed_north", FloatDomain(i->second.second.speed()[0]));
+        obs.restrictAttribute("speed_east", FloatDomain(i->second.second.speed()[1]));
+      }
+      notify(obs);
     }
-    notify(obs);
   }
   return true;
 }
