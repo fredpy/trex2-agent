4b3f71fa896e84f7b96372dd1f4b42bf9b963b81
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-05-24 04:50:11 +0000

    Added a quick implementation of the Drifter tracking app within trex2:   [doradoHandler] provides drifterFollow timeline statically (need to change in the future) within DrifterTracker amp based reactor   [DTAReactor] allow to set the type of survey (path, drifter, size) and post it as an email in sbd format

--- mbari/tracker/DoradoHandler.cc
+++ mbari/tracker/DoradoHandler.cc
@@ -3,9 +3,12 @@
 
 #include <trex/domain/IntegerDomain.hh>
 #include <trex/domain/FloatDomain.hh>
+#include <trex/domain/BooleanDomain.hh>
+#include <trex/domain/EnumDomain.hh>
 
 # define DATA_TL "trex_data"
 # define MBFD_TL "mbfd"
+# define TREX_TL "drifterFollow"
 
 using namespace mbari;
 using namespace TREX::transaction;
@@ -15,11 +18,18 @@ namespace {
 }
 
 DoradoHandler::DoradoHandler(MessageHandler::xml_arg const &arg) 
-  :MessageHandler(arg), m_updated(false) {
+  :MessageHandler(arg), m_updated(false), 
+  m_last_obs(TREX_TL, "undefined"), m_obs_fresh(false) {
   provide(DATA_TL);
   notify(TREX::transaction::Observation(DATA_TL, "undefined"));
   provide(MBFD_TL, true);
   notify(TREX::transaction::Observation(MBFD_TL, "Inactive"));
+  provide(TREX_TL);
+  notify(m_last_obs);
+    
+
+  m_filters.insert(std::make_pair("temperature", kalman_filter_fn(1.0, 1.0, .00024, .05)));
+  m_filters.insert(std::make_pair("salinity", kalman_filter_fn(1.0, 1.0, .00012, .05)));
 }
 
 bool DoradoHandler::handleRequest(TREX::transaction::goal_id const &g) {
@@ -60,6 +70,45 @@ bool DoradoHandler::handleMessage(amqp::queue::message &msg) {
       error_rate[1] = fix.easting_error_rate();
       m_serie.align(fix.from_time(), fix.to_time(), error_rate);
     }
+    for(size_t i=0; i<data.observations_size(); ++i) {
+      org::mbari::trex::Predicate const &pred(data.observations(i));
+      if( TREX_TL==pred.object() ) {
+        time_t date = pred.start();
+        
+        if( m_obs_fresh && m_since>date )
+          continue; // ignore old observations
+        m_last_obs = Observation(pred.object(), pred.predicate());
+        m_obs_fresh = true;
+        m_since = date;
+        // Add token attributes :
+        for(int v=0; v<pred.attributes_size(); ++v) {
+          org::mbari::trex::Predicate_Variable const &var = pred.attributes(v);
+          std::string name = var.name();
+          if( var.has_int_d() ) {
+            org::mbari::trex::Predicate_FloatDomain const &dom = var.int_d();
+            FloatDomain::bound lo(FloatDomain::minus_inf), 
+                                hi(FloatDomain::plus_inf);
+            
+            if( dom.has_min_v() )
+              lo = dom.min_v();
+            if( dom.has_max_v() )
+              hi = dom.max_v();
+            m_last_obs.restrictAttribute(name, FloatDomain(lo, hi));
+          } else if( var.has_bool_d() ) {
+            m_last_obs.restrictAttribute(name, BooleanDomain(var.bool_d()));
+          } else {
+            size_t enums = var.enum_d_size();
+            if( enums>0 ) {
+              EnumDomain values;
+              for(size_t e=0; e<enums; ++e)
+                values.add(var.enum_d(e));
+              m_last_obs.restrictAttribute(name, values);
+            }
+          }
+        }
+      }
+    }
+    
     m_updated = true;
     return true;
   }
@@ -68,6 +117,7 @@ bool DoradoHandler::handleMessage(amqp::queue::message &msg) {
 
 bool DoradoHandler::synchronize() {
   if( m_updated ) {
+    // Set all the 
     TREX::transaction::Observation obs(DATA_TL, "Received");
     double delta_t = std::floor(tickToTime(now()));
     delta_t -= m_serie.newest();
@@ -75,6 +125,17 @@ bool DoradoHandler::synchronize() {
     obs.restrictAttribute("freshness", IntegerDomain(std::floor(delta_t+0.5)));
     obs.restrictAttribute("nsamples", IntegerDomain(m_serie.size()));
     notify(obs);
+    
+    if( m_obs_fresh ) {
+      // Received an observcation from trex/dorado
+      delta_t = std::floor(tickToTime(now()));
+      delta_t -= m_since;
+      delta_t /= tickDuration();
+      // indicate how aold this observation is in term of ticks
+      m_last_obs.restrictAttribute("freshness", IntegerDomain(std::floor(delta_t+0.5)));
+      notify(m_last_obs);
+      m_obs_fresh = false;
+    }
   }
   m_updated = false;
   return true;
