cd8599b94e8ac42e509d05a8bed452287376575c
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-06-19 20:07:23 +0000

    Corrected a bug that prevented to dispatch token that could end on the next tick

--- trunk/extra/europa/core/CurrentState.cc
+++ trunk/extra/europa/core/CurrentState.cc
@@ -309,23 +309,37 @@ void CurrentState::do_dispatch(EUROPA::eint lb, EUROPA::eint ub) {
     i = timeline()->getTokenSequence().begin(),
     endi = timeline()->getTokenSequence().end();
 
-  // skip the past tokens
-  for( ; endi!=i && (*i)->end()->lastDomain().getLowerBound()<=(lb+1); ++i);
+  debugMsg("trex:dispatch", "Checking dispatch for "<<timeline()->toString()<<" ["<<lb<<", "<<ub
+    <<"]\n\t"<<timeline()->getTokenSequence().size()<<" tokens to check.");
 
+  // skip the past tokens
+  for( ; endi!=i && (*i)->start()->lastDomain().getUpperBound()<lb; ++i) {
+    debugMsg("trex:dispatch", "skipping "<<(*i)->toString()<<" as it ends to early (end="
+      <<(*i)->end()->lastDomain().toString()<<"<="<<(lb+1));
+  }
+  if( i!=endi )
+    debugMsg("trex:dispatch", "First token is "<<(*i)->toString()<<" start="<<(*i)->start()->lastDomain().toString());
 
   for( ; endi!=i && (*i)->start()->lastDomain().getLowerBound()<=ub; ++i) {
 #ifdef  EUROPA_HAVE_EFFECT
+    debugMsg("trex:dispatch", "Checking if token "<<(*i)->toString()
+        <<" overlaps ["<<lb<<", "<<ub<<"] as a goal (start="<<(*i)->start()->lastDomain().toString()); 
     // New code from Philip 
     // Check if next candidate is goal dependent ... needs to be refined I think
     EUROPA::TokenId nowGoal = getGoal(*i, lb, ub);
     if(nowGoal.isId())
         m_assembly.dispatch(timeline(),*i);
 #else 
+    debugMsg("trex:dispatch", "Checking if token "<<(*i)->toString()
+        <<" overlaps ["<<lb<<", "<<ub<<"] (start="<<(*i)->start()->lastDomain().toString()); 
     ///Old code for dispatching tokens
     // very dumb but work with europa 2.5
     if( !m_assembly.dispatch(timeline(), *i)
-       && (*i)->start()->lastDomain().getLowerBound()>=lb )
+       && (*i)->start()->lastDomain().getLowerBound()>=lb ) {
+      debugMsg("trex:dispatch", "Token "<<(*i)->toString()<<" cannot be dispatched and starts after "
+        <<lb<<"\n\t=>stopping dispatch for "<<timeline()->toString());
       break;
+    }
 #endif // EUROPA_HAVE_EFFECT
   }
 }
