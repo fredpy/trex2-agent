ec7c195888e35660ccfd45a223a4d0ae11f9c38e
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-06-27 22:10:37 +0000

    documented most of the TextLog public interfaces

--- trunk/trex/utils/TextLog.hh
+++ trunk/trex/utils/TextLog.hh
@@ -64,29 +64,117 @@ namespace TREX {
 
     namespace internals {
 
+      /** @brief log entry
+       * 
+       * This class is used as the basic container of a log entry 
+       * as it is produced through an entry_sink. It maintains 
+       * the TextLog that created it along with an optional creation 
+       * date, a source and a kind along with the message as it is is 
+       * build.
+       *
+       * When it is destroyed it will send all the informationto the 
+       * TextLog that can then consider it as a complete entry to be 
+       * sent to its listeners
+       *
+       * @relates TextLog
+       * @ingroup transaction
+       * @author Frederic Py
+       */
       class entry {
       public:
+	/** @brief identifiers type
+	 * 
+	 * The type used for both the source and kind of an entry
+	 */
         typedef Symbol                id_type;
+	/** @brief message type
+	 *
+	 * The type used to store the message content of en entry
+	 */
         typedef std::string           msg_type;
+	/** @brief date type
+	 *
+	 * The type used for a data when available.
+	 *
+	 * @note for convenience we used a type which correspoinf to  
+	 * a TREX TICK. A more generic implementation would probabaly 
+	 * use a more general type to represent a real date
+	 */
         typedef unsigned long long    date_type;
         typedef msg_type::value_type  char_type;
         typedef msg_type::size_type   size_type;
 
+	/** @brief destructor 
+	 *
+	 * If the message content is not empty. This method will 
+	 * notify the TextLog of the message. 
+	 *
+	 * @sa TextLog::send
+	 */
         ~entry();
+	/** @brief append new content
+	 *
+	 * @param s A pointer to the characters to append
+	 * @param n The number of character to append from @p s
+	 *
+	 * This method is used by entry_sink in order to append new content 
+	 * to the message of this entry
+	 *
+	 * @return @p n 
+	 */
         size_type write(char_type const *s, size_type n) {
           m_msg.append(s, n);
           return n;
 	}
         
       private:
+	/** @brief Creator 
+	 *
+	 * The TextLog that created this entry
+	 */
         TextLog &m_dest;
+	/** @brief date field
+	 *
+	 * The optional date of creation
+	 */
         boost::optional<date_type> m_date;
+	/** @brief source field
+	 *
+	 * The source of the entry
+	 */
         Symbol m_source;
+	/** @brief message kind
+	 *
+	 * The kind of the entry
+	 */
         Symbol m_kind;
+	/** @brief message content
+	 *
+	 * The mesage content of this entry
+	 */
         std::string m_msg;
 
+	/** @brief Constructor 
+	 *
+	 * @param dest A destination TextLog
+	 * @param src A source identifier
+	 * @param kind A message kind identifier
+	 *
+	 * Create a new entry of the type @p kind, to be sent to @p dest 
+	 * with no date and @p src as a source.
+	 */
         entry(TextLog &dest, Symbol const &src, Symbol const &kind)
         :m_dest(dest), m_source(src), m_kind(kind) {}
+	/** @brief Constructor 
+	 *
+	 * @param dest A destination TextLog
+	 * @param date A date 
+	 * @param src A source identifier
+	 * @param kind A message kind identifier
+	 *
+	 * Create a new entry of the type @p kind, to be sent to @p dest,
+	 * created on @p date and with @p src as a source.
+	 */
         entry(TextLog &dest, date_type const &date,
             Symbol const &src, Symbol const &kind)
         :m_dest(dest), m_date(date), m_source(src), m_kind(kind) {}
@@ -94,19 +182,60 @@ namespace TREX {
         friend class TREX::utils::TextLog;
       }; 
 
-      class log_entry;
+      class LogEntry;
 
+      /** @brief entry streming utility 
+       *
+       * A class used as a proxy to build the message of an entry using 
+       * a standard output stream.
+       *
+       * @relates LogEntry
+       * @ingroup transaction
+       * @author Frederic Py
+       * @sa class entry
+       */
       class entry_sink {
       public:
         typedef entry::char_type           char_type;
         typedef boost::iostreams::sink_tag category;
 
+	/** @brief Copy constructor 
+	 *
+	 * @param[in] other Another instance 
+	 *
+	 * Create a new instance and transfert tjhe ownership of the 
+	 * entry formerly associated to @p other to this new instance.
+	 *
+	 * This tranfert of ownership ensure that there will be only one 
+	 * stream writing to a given @p entry.
+	 *
+	 * @post other is not associated to an entry anymore
+	 */
         entry_sink(entry_sink const &other)
         :m_entry(other.m_entry) {}
+	/** @brief destructor
+	 *
+	 * If the entry is ossaciated to an entry it will destroy it, 
+	 * resulting on a new message send ot the entry TextLog
+	 *
+	 * @sa entry::~entry()
+	 */
         ~entry_sink() {
           m_entry.reset();
         }
 
+	/** @brief write operation
+	 *
+	 * @param s A pointer to the characters to append
+	 * @param n The number of character to append from @p s
+	 *
+	 * If this instance is associated to en entry it will write the 
+	 * @p n characters pointed by @p s to this entry. Otherwise it will 
+	 * do nothing.
+	 *
+	 * @retval @p n if the insatnce is associated to en entry
+	 * @retval 0 otherwise
+	 */
         std::streamsize write(char_type const *s, std::streamsize n) {
           if( NULL!=m_entry.get() )
             return m_entry->write(s, n);
@@ -116,24 +245,98 @@ namespace TREX {
       private:
         mutable std::auto_ptr<entry> m_entry;
 
+	/** @brief Constructor
+	 *
+	 * @param e A pointer to an entry
+	 *
+	 * Create a new instance  associated to the entry pointed by @p e
+	 */
         explicit entry_sink(entry *e):m_entry(e) {}
 
         friend class LogEntry;
       };
 
 
+      /** @brief A temporary local stream
+       *
+       * This class implements and maintain a stream to build a new entry
+       * to be sent to a TextLog. As the only public constructor of this 
+       * class is a "copy" constructor that transfert ownershipe of the 
+       * stream to the newly created entry it is a temproaray stream that 
+       * will release a new entry as soon as its last instance is destroyed.
+       *
+       * It is used as the core mechanism to produces new entries which are 
+       * atomically sent to the TextLog while apprently using a typical 
+       * stream.
+       *
+       * @relates TextLog
+       * @ingroup transaction
+       * @author Frederic Py 
+       */
       class LogEntry {
       public:
+	/** @brief subjacent stream type
+	 *
+	 * The standard output stream used to implement this class.
+	 * This stream is based on entry_sink 
+	 *
+	 * @sa entry_sink
+	 */
         typedef boost::iostreams::stream<entry_sink> stream_type;
 
+	/** @brief Copy constructor
+	 *
+	 * @param[in] other another instance 
+	 *
+	 * Create a new instance that takes control of the entry (if any)
+	 * @p other was writing to.
+	 *
+	 * @post @p other is no more associated to en entry
+	 */
         LogEntry(LogEntry const &other) :m_stream(*other.m_stream) {}
 
+	/** @brief Output stream operator
+	 *
+	 * @tparam Ty A type
+	 * @param x An insatnce of @p Ty
+	 * 
+	 * Call the operator @c << of the subjacent stream of this 
+	 * entry with @p x as an argument. 
+	 *
+	 * This aloow to make this instance appear like a regular 
+	 * stream to the caller.
+	 *
+	 * @return the subjacent stream of this instance 
+	 */
         template<typename Ty>
         std::ostream &operator<<(Ty const &x) {
           return m_stream<<x;
         }
 
+	/** @brief subjacent stream
+	 *
+	 * @return he stream maintained by this instance
+	 */
+	std::ostream &stream() const {
+	  return m_stream;
+	}
+	/** @brief implicit ostream ocnversion
+	 *
+	 * This operator allow to convert a LogEntry to a 
+	 * standard output stream.
+	 */
+	operator std::ostream &() const {
+	  return stream();
+	}
+
       private:
+	/** @brief Constructor 
+	 *
+	 * @param ref An entry
+	 *
+	 * Create a new instance associated to the entry pointed by 
+	 * @p ref.
+	 */
         LogEntry(entry *ref)
         :m_stream(entry_sink(ref)) {}
 
@@ -146,11 +349,53 @@ namespace TREX {
       };
     } // TREX::utils::internals
 
+    /** @brief Empty symbol
+     *
+     * An  empty symbol used for entries with no type
+     *
+     * @relates TextLog
+     */
     extern Symbol const null;
+    /** @brief info symbol
+     *
+     * A symbol used as the kind for info entries
+     *
+     * @relates TextLog
+     */
     extern Symbol const info;
+    /** @brief Warning symbol
+     *
+     * A symbol used as the kind for warning entries 
+     *
+     * @relates TextLog
+     */
     extern Symbol const warn;
+    /** @brief Error symbol
+     *
+     * A symbol used as the kind for error entries 
+     *
+     * @relates TextLog
+     */
     extern Symbol const error;
 
+    /** @brief Logging text entries management class
+     *
+     * This class implements the core mechanism for trex to log
+     * text messages that are atomic, relativeley thread safe and 
+     * can be dispatched to multiple listeners. All the messages (or 
+     * entry) have an associated source, kind and optianally a date
+     *
+     * The entries are built by the programmer using standard C++ 
+     * streams that when released will produce a new entry that this 
+     * class will then send to a primary listener and eventually dispatch 
+     * to exsting secondary listeners that are ran in another thread.
+     *
+     * All the syslog() methods found accros TREX classes are supported 
+     * by this class.
+     *
+     * @ingroup transaction
+     * @euthor Frederic Py
+     */
     class TextLog {
     public:
       typedef internals::entry::id_type   id_type;
@@ -158,12 +403,56 @@ namespace TREX {
       typedef internals::entry::date_type date_type;
       typedef internals::LogEntry         stream_type;
 
+      /** @brief Default constructor 
+       *
+       * Create a new instance with no primary handler
+       */
       TextLog() {}
+      /** @brief Constructor
+       *
+       * @tparam Handler a handler type
+       * @param[in] primary An instance of @p Handler
+       *
+       * Create a new instance that will use a copy of @p primary
+       * as its primary handler.
+       *
+       * @pre Handler is copy contructible
+       * @pre Handler derives from handler
+       *
+       * @note It is hgihly recommended to have Handler class to be thread 
+       * safe with is message method protected against reentrance. Indeed 
+       * the primary handler of a TextLog can be called an any threads and 
+       * concurrently.
+       */
       template<class Handler>
       TextLog(Handler const &primary)
       :m_primary(new Handler(primary)) {}
+      /** @brief Destructor 
+       * 
+       * destroy this instance and all the handlers associated to it.
+       */
       ~TextLog();
 
+      /** @brief set primary handler
+       *
+       * @tparam Handler a handler type
+       * @param[in] primary An instance of @p Handler
+       *
+       * If this instance does not yet have a primary handler, this call 
+       * make a copy of @p primary as its primary handler.
+       *
+       * @pre Handler is copy contructible
+       * @pre Handler derives from handler
+       *
+       * @retval true if this call resulted on a copy of @p handler becoming
+       * this instance primary handler.
+       * @retval false if this instance had already a rpimary handler
+       *
+       * @note It is highly recommended to have Handler class to be thread 
+       * safe with is message method protected against reentrance. Indeed 
+       * the primary handler of a TextLog can be called an any threads and 
+       * concurrently.
+       */
       template<class Handler>
       bool set_primary(Handler const &primary) {
         if( NULL==m_primary.get() ) {
@@ -173,6 +462,25 @@ namespace TREX {
         return false;
       }
 
+      /** @brief Ad secondary handler
+       *
+       * @tparam Handler a handler type
+       * @param handle An instance of Handler
+       *
+       * Add a copy of @p handle as a secondary handler of this instance
+       *
+       * @pre Handler is copy contructible
+       * @pre Handler derives from handler
+       *
+       * @post A copy of @p handle is now associated to this TextLog that
+       * will notify it of new entries within a specific thread created by 
+       * this instance for secondary handlers.
+       *
+       * @note As all the call of message for secondary handler are made in 
+       * a specific thread this call does not need to be protected against 
+       * reentrance for @p Handler. Do note also that as opposed to the primary 
+       * handler the notification of nrew entries may be delayed.
+       */
       template<class Handler>
       void add_handler(Handler const &handle) {
         std::auto_ptr<handler> h(new Handler(handle));
@@ -191,48 +499,97 @@ namespace TREX {
         }
       }
 
-      stream_type msg(id_type const &from, id_type const &kind=null) {
-        return stream_type(new internals::entry(*this, from, kind));
-      }
+      /** @brief new entry
+       * 
+       * @param[in] date date of the entry
+       * @param[in] from source of the entry
+       * @param[in] kind type of the entry
+       *
+       * Create a new empty entry that can be fileld using standard output 
+       * stream operations
+       *
+       * @return A temporary stream to build this entry
+       * @{
+       */
       stream_type msg(date_type const &when, 
           id_type const &from, id_type const &kind=null) {
         return stream_type(new internals::entry(*this, when, from, kind));
-      }
-      stream_type operator()(id_type const &from, 
-          id_type const &kind=null) {
-        return msg(from, kind);
       }
       stream_type operator()(long long when,
           id_type const &from,
           id_type const &kind=null) {
         return msg(when, from, kind);
       }
+      stream_type msg(id_type const &from, id_type const &kind=null) {
+        return stream_type(new internals::entry(*this, from, kind));
+      }
+      stream_type operator()(id_type const &from, 
+          id_type const &kind=null) {
+        return msg(from, kind);
+      }
+      /** @} */
 
+      /** @brief Log entry handler
+       *
+       * The base class used to implement any TextLog entry handler. 
+       *
+       * A handler is just a class that will be notified of new entries 
+       * created and can then process them. Ty[pical processing would be 
+       * to redirect them into a stream
+       *
+       * @relates TextLog
+       * @ingroup transaction
+       * @author Frederic Py 
+       */
       class handler {
       public:
         typedef TextLog::id_type   id_type;
         typedef TextLog::msg_type  msg_type;
         typedef TextLog::date_type date_type;
 
+	/** @brief default constructor */
         handler()
         :m_log(NULL) {}
+	/** @brief Copy constructor */
         handler(handler const &other)
         :m_log(NULL) {}
-        virtual ~handler() {
-          detach();
-        }
+	/** @brief Destructor 
+	 *
+	 * If the handler was associated to a TextLog, 
+	 * notifies the TextLog about its destruction
+	 */
+        virtual ~handler();
 
       protected:
+	/** @brief Check if associated to a TextLog
+	 *
+	 * @retval true if associated to a TextLog
+	 * @retval false otherwise
+	 */
         bool is_attached() {
           return NULL!=m_log;
         }
+	/** @brief Detach from TextLog
+	 *
+	 * This method allow to detach the handler from its associated 
+	 * TextLog when possible.
+	 * @note As of now we do not allow to detach a primary handler 
+	 * from it
+	 * @retval true if succesfully detached
+	 * @retval false otherwise
+	 */
         bool detach();
 
+	/** @brief New entry
+	 *
+	 * This callback notifeds attached handlers from a new message
+	 */
         virtual void message(boost::optional<date_type> const &date,
             id_type const &who, id_type const &kind,
             msg_type const &what) =0;
 
       private:	
+        bool detach(bool);
         TextLog *m_log;
 
         friend class TextLog;
