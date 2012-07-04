acb999231bdaefdbdc343cf25d83091a039fee9c
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-07-04 20:57:42 +0000

    Corrected our threat handler to not filter lout the past if the tokens necessarily starts in the past

--- extra/europa/core/TrexThreatDecisionPoint.cc
+++ extra/europa/core/TrexThreatDecisionPoint.cc
@@ -33,39 +33,48 @@ TrexThreatDecisionPoint::TrexThreatDecisionPoint
 void TrexThreatDecisionPoint::handleInitialize() {
   ThreatDecisionPoint::handleInitialize();
   
-  EUROPA::eint min_dur = m_tokenToOrder->duration()->lastDomain().getLowerBound();
-  // Prune all the solutions taht would put this token in the past
-  std::vector<std::pair<ObjectId, std::pair<TokenId, TokenId> > >::iterator i = m_choices.begin();
-  while( m_choices.end()!=i ) {
-    if( m_tokenToOrder==i->second.first ) {
-      if( m_tokenToOrder!=i->second.second ) {
-	// m_tokenToOrder inserted before i->second.second
-	EUROPA::eint max_start = i->second.second->start()->lastDomain().getUpperBound();
-	if( max_start<=std::numeric_limits<EUROPA::eint>::infinity() ) {
-	  max_start = max_start-min_dur;
-	  if( max_start < now() ) {
-	    debugMsg("trex:threat", "remove (*"<<m_tokenToOrder->getKey()<<"*<"
-		     <<i->second.second->getKey()<<")\n\tthe latter start="
-		     <<i->second.second->start()->lastDomain().toString()
-		     <<" is before "<<now());
-	    i = m_choices.erase(i);
-	    continue;
+  if( m_tokenToOrder->start()->lastDomain().getUpperBound()>=now() ) { 
+    EUROPA::eint min_dur = m_tokenToOrder->duration()->lastDomain().getLowerBound();
+    // Prune all the solutions taht would put this token in the past
+    std::vector<std::pair<ObjectId, std::pair<TokenId, TokenId> > >::iterator i = m_choices.begin();
+    while( m_choices.end()!=i ) {
+      if( m_tokenToOrder==i->second.first ) {
+	if( m_tokenToOrder!=i->second.second ) {
+	  // m_tokenToOrder inserted before i->second.second
+	  EUROPA::eint max_start = i->second.second->start()->lastDomain().getUpperBound();
+	  if( max_start<=std::numeric_limits<EUROPA::eint>::infinity() ) {
+	    max_start = max_start-min_dur;
+	    if( max_start < now() ) {
+	      debugMsg("trex:threat", "remove (*"<<m_tokenToOrder->getKey()<<"*<"
+		       <<i->second.second->getKey()<<")\n\tthe latter start="
+		       <<i->second.second->start()->lastDomain().toString()
+		       <<" is before "<<now());
+	      i = m_choices.erase(i);
+	      continue;
+	    }
 	  }
-	}
-      } 
-    } else {
-      // m_tokenToOrder is inserted after i->second.first 
-      // check that i->second.first ends after now()
-      if( i->second.first->end()->lastDomain().getUpperBound()<now() ) {
-	debugMsg("trex:threat", "remove ("<<i->second.first->getKey()<<"<*"
-		 <<m_tokenToOrder->getKey()<<"*)\n\tthe former end="
-		 <<i->second.second->end()->lastDomain().toString()
-		 <<" is before "<<now());
-	i = m_choices.erase(i);
-	continue;
-      }	
+	} 
+      } else {
+	// m_tokenToOrder is inserted after i->second.first 
+	// check that i->second.first ends after now()
+	if( i->second.first->end()->lastDomain().getUpperBound()<now() ) {
+	  debugMsg("trex:threat", "remove ("<<i->second.first->getKey()<<"<*"
+		   <<m_tokenToOrder->getKey()<<"*)\n\tthe former end="
+		   <<i->second.second->end()->lastDomain().toString()
+		   <<" is before "<<now());
+	  i = m_choices.erase(i);
+	  continue;
+	}	
+      }
+      ++i;
     }
-    ++i;
+  } else {
+    debugMsg("trex:threat", m_tokenToOrder->getKey()<<" start="
+	     <<m_tokenToOrder->start()->lastDomain().toString()
+	     <<" is before "<<now()<<"\n\tdo keep its options open.");
+    // Mayebe we should exclude all choices that do not put start 
+    // in the past ? ... it should have been already done by the 
+    // other guy
   }
   m_choiceCount = m_choices.size();
 }
