4f44f92a1ca83990cd0d78f2de24181ac0cfbbbb
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-07-05 17:35:21 +0000

    [amc] Added the ability to set amc priority based on TREX_NICE environment variable

--- trex/cmds/AMC.cc
+++ trex/cmds/AMC.cc
@@ -81,6 +81,8 @@
 #include <trex/agent/RealTimeClock.hh>
 #include <trex/agent/StepClock.hh>
 
+#include "nice_flags.h"
+
 using namespace TREX::agent;
 using namespace TREX::utils;
 
@@ -158,7 +160,45 @@ int main(int argc, char **argv) {
   signal(SIGTERM, amc_cleanup);
   signal(SIGQUIT, amc_cleanup);
   signal(SIGKILL, amc_cleanup);
-  
+  // force trex log initialization
+  amc_log->logPath();
+
+  amc_log->syslog("amc", info)<<"Checking for TREX_NICE";
+  char * priority_env = getenv("TREX_NICE");
+  if( NULL!=priority_env ) {
+
+    amc_log->syslog("amc", info)<<"Found environment TREX_NICE="<<priority_env;
+    unsigned priority;
+    try {
+      int ret;
+      priority = TREX::utils::string_cast<unsigned>(priority_env);
+#ifdef HAVE_SETPRIORITY
+      int which = PRIO_PROCESS;
+      id_t pid;
+
+      pid = getpid();
+      amc_log->syslog("amc", info)<<"Setting my priority to "<<priority;
+      ret =  setpriority(which, pid, priority);
+      if( ret<0 )
+	amc_log->syslog("amc", error)<<"Error while trying to set the priority:\n\t"
+			    <<strerror(errno);
+#else
+# ifdef HAVE_NICE
+      amc_log->syslog("amc", info)<<"Nicing myself to "<<priority;
+      ret = nice(priority);
+      if( ret<0 )
+	amc_log->syslog("amc", error)<<"Error while trying to set the priority:\n\t"
+			    <<strerror(errno);
+# else 
+      amc_log->syslog("amc", error)<<"Don't know how to change my priority on this system.";
+# endif
+#endif 
+    } catch(TREX::utils::bad_string_cast const &e) {
+      amc_log->syslog("amc", error)<<"Failed to parse TREX_NICE as an unsigned:\n"
+				   <<e.what();
+    }
+  }
+ 
   my_agent.reset(new Agent(configFile, clk.release()));
   // Use a 1Hz clock by default
   my_agent->setClock(new RealTimeClock(boost::chrono::seconds(1)));
