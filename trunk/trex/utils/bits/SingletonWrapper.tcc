c84e98ab26397f0be23c3ddbae078782c1e52872
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-10-19 03:07:05 +0000

    [utils] cosmetic indentation update

--- trunk/trex/utils/bits/SingletonWrapper.tcc
+++ trunk/trex/utils/bits/SingletonWrapper.tcc
@@ -3,18 +3,18 @@
  * @brief SingletonWrapper implementation
  *
  * @author Frederic Py <fpy@mbari.org>
- * @ingroup utils 
+ * @ingroup utils
  */
 /*********************************************************************
  * Software License Agreement (BSD License)
- * 
+ *
  *  Copyright (c) 2011, MBARI.
  *  All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
- * 
+ *
  *   * Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
@@ -24,7 +24,7 @@
  *   * Neither the name of the TREX Project nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -40,53 +40,53 @@
  */
 #ifndef In_H_SingletonWrapper
 # error "Cannot include tcc file outside of its corresponding header"
-#else 
+#else
 
 #include <typeinfo>
 
 namespace TREX {
   namespace utils {
-
+    
     namespace internal {
-
+      
       template<typename Ty>
       struct swrapper_factory :public sdummy_factory {
-	SingletonDummy *create() const {
-	  return new SingletonWrapper<Ty>;
-	}
-      }; 
-
+        SingletonDummy *create() const {
+          return new SingletonWrapper<Ty>;
+        }
+      };
+      
     }
-
+    
     // statics :
     
     template<typename Ty>
     Ty *SingletonWrapper<Ty>::attach() {
       SingletonWrapper<Ty> *
-	me = static_cast<SingletonWrapper<Ty> *>(internal::SingletonDummy::attach(name(), 
-                                                                                  internal::swrapper_factory<Ty>()));
+      me = static_cast<SingletonWrapper<Ty> *>(internal::SingletonDummy::attach(name(),
+                                                                                internal::swrapper_factory<Ty>()));
       return &(me->m_value);
     }
-
+    
     template<typename Ty>
     void SingletonWrapper<Ty>::detach() {
       internal::SingletonDummy::detach(name());
     }
-
+    
     template<typename Ty>
     std::string SingletonWrapper<Ty>::name() {
       return typeid(Ty).name();
     }
-
+    
     // structors :
-
+    
     template<typename Ty>
     SingletonWrapper<Ty>::SingletonWrapper() {}
-
+    
     template<typename Ty>
     SingletonWrapper<Ty>::~SingletonWrapper() {}
-
+    
   }
 }
 
-#endif  
+#endif
--- trunk/trex/utils/chrono_helper.hh
+++ trunk/trex/utils/chrono_helper.hh
@@ -1,13 +1,13 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
- * 
+ *
  *  Copyright (c) 2011, MBARI.
  *  All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
- * 
+ *
  *   * Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
  *   * Neither the name of the TREX Project nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -40,66 +40,66 @@
 # include <iostream>
 
 namespace TREX {
-    namespace utils {
+  namespace utils {
+    
+    /** @brief Human readable duration display
+     * @tparam CharT stream char type
+     * @tparam Traits stream char traits
+     * @tparam Rep duration base type
+     * @tparam Period duration precision period
+     *
+     * @param[in,out] out An output stream
+     * @param[in] d A duration
+     *
+     * Serialize the value of the duration @p d into @p out as an
+     * "human readable" value.
+     *
+     * @return @p out after the operation
+     */
+    template <class CharT, class Traits, class Rep, class Period>
+    std::basic_ostream<CharT, Traits> &display(std::basic_ostream<CharT, Traits> &out,
+                                               boost::chrono::duration<Rep, Period> d) {
+      if( d < boost::chrono::duration<Rep, Period>::zero() ) {
+        d = -d;
+        out.put('-');
+      }
+      // Extract the number of seconds
+      boost::chrono::seconds
+      s = boost::chrono::duration_cast<boost::chrono::seconds>(d);
+      
+      if( s>boost::chrono::seconds::zero() ) {
+        d -= s;
         
-        /** @brief Human readable duration display
-         * @tparam CharT stream char type
-         * @tparam Traits stream char traits
-         * @tparam Rep duration base type
-         * @tparam Period duration precision period
-         *
-         * @param[in,out] out An output stream
-         * @param[in] d A duration
-         *
-         * Serialize the value of the duration @p d into @p out as an
-         * "human readable" value.
-         *
-         * @return @p out after the operation
-         */
-        template <class CharT, class Traits, class Rep, class Period>
-        std::basic_ostream<CharT, Traits> &display(std::basic_ostream<CharT, Traits> &out,
-                                                   boost::chrono::duration<Rep, Period> d) {
-            if( d < boost::chrono::duration<Rep, Period>::zero() ) {
-                d = -d;
-                out.put('-');
-            }
-            // Extract the number of seconds
-            boost::chrono::seconds
-            s = boost::chrono::duration_cast<boost::chrono::seconds>(d);
-            
-            if( s>boost::chrono::seconds::zero() ) {
-                d -= s;
-                
-                // Extract number of minutes
-                boost::chrono::minutes
-                m = boost::chrono::duration_cast<boost::chrono::minutes>(s);
-                
-                if( m > boost::chrono::minutes::zero() ) {
-                    s -= m;
-                    
-                    // Extract the number of hours
-                    boost::chrono::hours
-                    h = boost::chrono::duration_cast<boost::chrono::hours>(m);
-                    
-                    if( h > boost::chrono::hours::zero() ) {
-                        m -= h;
-                        // Display as hours:MM:SS.XXXX
-                        out<<h.count()<<':';
-                        if( m < boost::chrono::minutes(10) )
-                            out.put('0'); // Need 2 digits
-                    }
-                    out<<m<<':';
-                    if( s < boost::chrono::seconds(10) )
-                        out.put('0'); // Need 2 digits
-                }
-            } 
-            typedef boost::chrono::duration<long double> f_secs;
-            f_secs ss = boost::chrono::duration_cast<f_secs>(s);
-            ss += d;
-            return boost::chrono::duration_short(out)<<ss;
+        // Extract number of minutes
+        boost::chrono::minutes
+        m = boost::chrono::duration_cast<boost::chrono::minutes>(s);
+        
+        if( m > boost::chrono::minutes::zero() ) {
+          s -= m;
+          
+          // Extract the number of hours
+          boost::chrono::hours
+          h = boost::chrono::duration_cast<boost::chrono::hours>(m);
+          
+          if( h > boost::chrono::hours::zero() ) {
+            m -= h;
+            // Display as hours:MM:SS.XXXX
+            out<<h.count()<<':';
+            if( m < boost::chrono::minutes(10) )
+              out.put('0'); // Need 2 digits
+          }
+          out<<m<<':';
+          if( s < boost::chrono::seconds(10) )
+            out.put('0'); // Need 2 digits
         }
-        
-    } // TREX::utils
-} // TREX 
+      }
+      typedef boost::chrono::duration<long double> f_secs;
+      f_secs ss = boost::chrono::duration_cast<f_secs>(s);
+      ss += d;
+      return boost::chrono::duration_short(out)<<ss;
+    }
+    
+  } // TREX::utils
+} // TREX
 
 #endif // H_trex_utils_chrono_helper
