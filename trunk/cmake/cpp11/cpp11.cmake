7f1e853d199cbd0d48c4fa60f4c71934ee229501
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-11-15 17:34:34 +0000

    [cpp11] Added a test for unique_ptr

--- trunk/cmake/cpp11/cpp11.cmake
+++ trunk/cmake/cpp11/cpp11.cmake
@@ -1,3 +1,4 @@
+# -*- cmake -*-
 #  Copyright 2010-2012 Matus Chochlik. Distributed under the Boost
 #  Software License, Version 1.0. (See accompanying file
 #  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -49,7 +50,9 @@ endfunction()
 #cpp11_feature_detection(NOEXCEPT)
 #cpp11_feature_detection(LAMBDAS)
 #cpp11_feature_detection(NULLPTR)
+
 cpp11_feature_detection(CHRONO)
+cpp11_feature_detection(UNIQUE_PTR)
 
 # explicit configuration
 if(
