0d9a054b9fafffa999a432691bd4a9c0e39c44f4
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2012-12-06 18:59:53 +0000

    [ros] typos

--- trunk/extra/third_party/ros/ros_client.hh
+++ trunk/extra/third_party/ros/ros_client.hh
@@ -58,7 +58,7 @@ namespace TREX {
       
       void spin_cb();
       
-      TREX::utils::SingletonUse<TREX::utis::LogManager> m_log;
+      TREX::utils::SingletonUse<TREX::utils::LogManager> m_log;
       TREX::utils::SharedVar<bool> m_active;
       boost::asio::deadline_timer m_freq;
       
