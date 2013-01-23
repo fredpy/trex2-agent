123354764fabc3942e8a7543233ff59ea1b92c49
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-01-23 17:29:59 +0000

    [c++11] documented macro for unique pointers in memory.hh

--- trex/utils/platform/memory.hh
+++ trex/utils/platform/memory.hh
@@ -31,18 +31,57 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
+/** @file "trex/utils/platform/memory.hh"
+ * @brief C++11 Unique pointer adapter header
+ *
+ * This header provide macros that uses either std::auto_ptr or
+ * its C++11 counterpart std::unique_ptr when trex was compiled with 
+ * C++11 support
+ *
+ * It provide two macros that allow to handle unique pointers as if 
+ * their implementation was @e relativelly similar to C++11 specs. 
+ * Specifically it provides a macro to get the preferred type for 
+ * unique pointers and a macro to move the pointer from one instance 
+ * to another
+ *
+ * @note It is recommended to replace all references to the standard 
+ *   memory header by this header
+ * @author Frederic Py <fpy@mbari.org>
+ * @ingroup utils
+ */
 #ifndef H_trex_memory
 # define H_trex_memory
 
 # include <memory>
 # include "bits/cpp11.hh"
 
-# ifdef CPP11_HAS_UNIQUE_PTR
-#  define UNIQ_PTR  std::unique_ptr
-#  define STD_MOVE(ptr) std::move(ptr)
-# else // CPP11_HAS_UNIQUE_PTR
-#  define UNIQ_PTR  std::auto_ptr
-#  define STD_MOVE(ptr) ptr
-# endif // CPP11_HAS_UNIQUE_PTR
+# ifdef DOXYGEN
+/** @brief Unique pointer type
+ * The type to be used for unqie pointers. It is either std::auto_ptr 
+ * or std::unique_ptr depending on C++11 support
+ */
+#  define UNIQ_PTR computed_type
+/** @brief Unique pointer moving function
+ * 
+ * @param[in] ptr A @c UNIQ_PTR instance
+ *
+ * The "function" to call in order to move @p ptr to another instance. 
+ * Moving a unique pointer means that its ownership is transferred to
+ * another instance.
+ *
+ * Before C++11 this operation was done implicitelly which has been 
+ * corrected with the introduction of std::unique_ptr meant to 
+ * replace the dperecated std::auto_ptr
+ */
+#  define STD_MOVE(ptr)
+# else
+#  ifdef CPP11_HAS_UNIQUE_PTR
+#   define UNIQ_PTR  std::unique_ptr
+#   define STD_MOVE(ptr) std::move(ptr)
+#  else // CPP11_HAS_UNIQUE_PTR
+#   define UNIQ_PTR  std::auto_ptr
+#   define STD_MOVE(ptr) ptr
+#  endif // CPP11_HAS_UNIQUE_PTR
+# endif // DOXYGEN
 
 #endif // H_trex_memory
