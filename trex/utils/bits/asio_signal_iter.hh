cd4f918d97853ba242d916a57c18d1025cd77a8e
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-01-29 22:25:36 +0000

    Cleaned up the BOOST_PP* defined in this file

--- trex/utils/bits/asio_signal_iter.hh
+++ trex/utils/bits/asio_signal_iter.hh
@@ -51,7 +51,11 @@
 // Generate specializations for 0 to ASIOSIG_MAX_SIZE arguments
 #  define BOOST_PP_ITERATION_LIMITS (0, ASIOSIG_MAX_SIZE)
 #  define BOOST_PP_FILENAME_1       "trex/utils/bits/asio_signal_iter.hh"
+
 #  include BOOST_PP_ITERATE()
+
+#  undef BOOST_PP_ITERATION_LIMITS
+#  undef BOOST_PP_FILENAME_1
 
 # else // BOOST_PP_IS_ITERATING
 
