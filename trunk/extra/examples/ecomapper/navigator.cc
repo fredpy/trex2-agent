ae63a678a1ce661fba293300b452326c91b6ce09
Cooksey.Philip@gmail.com <Cooksey.Philip@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-02-11 01:37:23 +0000

    Ecomapper updates

--- trunk/extra/examples/ecomapper/navigator.cc
+++ trunk/extra/examples/ecomapper/navigator.cc
@@ -8,6 +8,11 @@ using namespace TREX::utils;
 using namespace TREX::transaction;
 using namespace TREX::ecomapper;
 
+static const std::string gLatitude("latitude"), gLongitude("longitude"),
+                         gDepth("depth"), gAltitude("altitude"),
+                         gTotal_Water_Column("Total_Water_Column");
+static const double gDepthThreshold(100);
+
 Navigator::Navigator(TeleoReactor::xml_arg_type arg)
     :TeleoReactor(arg,false)
 {
@@ -49,8 +54,8 @@ bool Navigator::synchronize()
 
 void Navigator::notify(TREX::transaction::Observation const &obs)
 {
-	///Dispatching the different timeline observations to their respective function 
-	TREX::utils::Symbol const & object = obs.object(); 
+	///Dispatching the different timeline observations to their respective function
+	TREX::utils::Symbol const & object = obs.object();
 	if(object==Ros_Listener::dvlObj)
 		dvlObservation(obs);
 	else if(object==Ros_Listener::ctd_rhObj)
@@ -61,7 +66,14 @@ void Navigator::notify(TREX::transaction::Observation const &obs)
 
 void Navigator::dvlObservation(TREX::transaction::Observation const &obs)
 {
-	
+    lock.lock();
+    double depth = getAttribute(gDepth, obs);
+    double altitude = getAttribute(gAltitude, obs);
+    addToMap(gDepth, depth);
+    addToMap(gAltitude, altitude);
+    //Still need to find the actual total_water_column
+    addToMap(gTotal_Water_Column,depth+altitude);
+    lock.unlock();
 }
 void Navigator::ctd_rhObservation(TREX::transaction::Observation const &obs)
 {
@@ -70,11 +82,42 @@ void Navigator::ctd_rhObservation(TREX::transaction::Observation const &obs)
 void Navigator::fixObservation(TREX::transaction::Observation const &obs)
 {
 	lock.lock();
-	//FloatDomain const & x= obs.getAttribute(Symbol("latitude")).typedDomain();
-	//FloatDomain y = obs.getDomain(Symbol("longitude"));
-	//This is were the point is add to the spline if depth is deep enough
-	//spline.addPoint(x,y);
-	lock.unlock(); 
+	double x = getAttribute(gLatitude, obs);
+    double y = getAttribute(gLongitude, obs);
+    bool nextPoint = (getMapValue(gLatitude)!=0)?true:false;
+    //If the depth is greater than the threshold add the inital point
+	if(!nextPoint && getMapValue(gTotal_Water_Column) >= gDepthThreshold)
+	{
+	    addToMap(gLatitude, x);
+        addToMap(gLongitude, y);
+	}
+	//If the dpeth is less than the treshold than we see
+	//if we already have a value which means we are at the end of the depth
+	else if(nextPoint)
+	{
+	    //We calculate the midpoint between the start and end points of the
+	    //side of the canyon we just completed
+	    double midx = (getMapValue(gLatitude)+x)/2;
+	    double midy = (getMapValue(gLongitude)+y)/2;
+	    //Add the point into the cubic spline
+	    spline.addPoint(midx, midy);
+	    //Then zero out the values
+	    addToMap(gLatitude, 0);
+	    addToMap(gLongitude, 0);
+	}
+	lock.unlock();
+}
+
+double Navigator::getAttribute(std::string const &name, Observation const &obs)
+{
+    double value;
+    try
+    {
+        value = boost::any_cast<double>(obs.getAttribute(Symbol(name)).domain().getSingleton());
+    } catch (boost::bad_any_cast& err) {
+        value = -1;
+    }
+    return value;
 }
 
 void Navigator::handleRequest(goal_id const &g)
