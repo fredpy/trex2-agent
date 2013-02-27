21ee988e59e229ff4c1383cacadf46899749cfb2
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-02-27 22:23:54 +0000

    [ptyhon] quick of the example python function

--- trunk/trex/python/example/amc.py
+++ trunk/trex/python/example/amc.py
@@ -3,6 +3,8 @@
 import trex
 
 def run_agent(file_name):
+    "batch execution of an agent"
+    "This call executes an agent based on the config sepcified in the file file_name"
     agent = trex.agent.agent(file_name)
     default_clock = trex.agent.rt_clock(1000)
     agent.set_clock(default_clock)
