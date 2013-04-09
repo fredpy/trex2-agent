188630f11f83118bc2d09addde83462937974d2c
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-04-09 01:43:40 +0000

    [sandbox] implemented a working and robust example of a clock based on asio … I need now to refine this with my signal merger

--- sandbox/src/multiplexer.cc
+++ sandbox/src/multiplexer.cc
@@ -1,58 +1,155 @@
-#include "trex/merged_signal.hh"
 #include "trex/asio_runner.hh"
+
+#include <boost/signals2.hpp>
 
 #include <iostream>
 #include <string>
 
 using namespace trex;
+namespace ba = boost::asio;
+namespace bs = boost::signals2;
 
-void tick(std::string const &who, int val) {
-  std::cout<<who<<" tick : "<<val<<std::endl;
+
+
+class ticker {
+public:
+  typedef int tick;
+  
+  typedef bs::signal<void (tick)>           tick_sig;
+  typedef ba::deadline_timer::duration_type duration_type;
+  
+  explicit ticker(ba::io_service &service, duration_type const &f);
+  ~ticker();
+  
+  bool active() const;
+  void start(int deadline=0);
+  void stop();
+  
+  void wait_completion();
+  
+  bs::connection on_tick(tick_sig::slot_type s);
+  bs::connection on_end(tick_sig::slot_type s);
+  
+  void initial_tick(tick v) {
+    m_tick(v);
+  }
+  void completion_task();
+  void ticker_fn(boost::system::error_code const &);
+  
+private:
+  ba::io_service &service() {
+    return m_strand.get_io_service();
+  }
+  
+  tick m_last_tick, m_deadline;
+  
+  boost::scoped_ptr< boost::packaged_task<void> > m_terminate_fn;
+  boost::unique_future<void> m_completed;
+  duration_type const m_freq;
+  
+  // asio things
+  ba::strand         m_strand;
+  ba::deadline_timer m_timer;
+  boost::scoped_ptr<ba::io_service::work> m_task;
+  
+  // signals2
+  tick_sig m_tick, m_last;
+};
+
+ticker::ticker(ba::io_service &service, duration_type const &f)
+:m_freq(f), m_strand(service), m_timer(service) {}
+
+ticker::~ticker() {
+  stop();
+  wait_completion();
 }
 
-void tick_incr(merged_signal::signal_type &s, int val) {
-  if( val>=4 )
-    s(val+1);
+bool ticker::active() const {
+#if 0
+  return (!m_completed.is_ready())
+  && boost::future_state::uninitialized!=m_completed.get_state();
+#else
+  return boost::future_state::waiting==m_completed.get_state();
+#endif
 }
 
+void ticker::start(int deadline) {
+  if( deadline>0 && !active() ) {
+    m_task.reset(new ba::io_service::work(service()));
+    m_last_tick = 0; m_deadline = deadline;
+    m_terminate_fn.reset(new boost::packaged_task<void>(boost::bind(&ticker::completion_task, this)));
+    m_completed = m_terminate_fn->get_future();
+    m_timer.expires_from_now(m_freq);
+    m_strand.post(boost::bind(boost::ref(m_tick), 0));
+    m_timer.async_wait(m_strand.wrap(boost::bind(&ticker::ticker_fn, this, _1)));
+  }
+}
+
+void ticker::stop() {
+  if( active() ) {
+    m_timer.cancel();
+  }
+}
+
+bs::connection ticker::on_tick(tick_sig::slot_type s) {
+  return m_tick.connect(s);
+}
+
+bs::connection ticker::on_end(tick_sig::slot_type s) {
+  return m_last.connect(s);
+}
+
+
+void ticker::wait_completion() {
+  if( active() ) {
+    m_completed.wait();
+  }
+}
+
+void ticker::completion_task() {
+  m_task.reset();
+  m_last(m_last_tick);
+}
+
+void ticker::ticker_fn(boost::system::error_code const &ec) {
+  if( boost::asio::error::operation_aborted!=ec &&
+     m_last_tick<m_deadline ) {
+    m_last_tick += 1;
+    m_timer.expires_at(m_timer.expires_at()+m_freq);
+    m_timer.async_wait(m_strand.wrap(boost::bind(&ticker::ticker_fn, this, _1)));
+    m_tick(m_last_tick);
+  } else /* if( m_terminate_fn ) */ {
+    (*m_terminate_fn)();
+  }
+}
+
+void new_tick(std::string name, int v) {
+  std::cout<<name<<" tick: "<<v<<" ("<<boost::this_thread::get_id()<<")"<<std::endl;
+}
+
+void end_of_clk(int v) {
+  std::cout<<"end: "<<v<<" ("<<boost::this_thread::get_id()<<")"<<std::endl;
+}
 
 int main() {
   asio_runner io;
+  ticker clk(io.service(), boost::posix_time::milliseconds(1));
   
+  clk.on_tick(boost::bind(&new_tick, "clk", _1));
+  clk.on_end(&end_of_clk);
   
-  merged_signal reactor(io.service());
-  reactor.connect(boost::bind(tick, "reactor", _1));
+  std::cout<<"Have "<<io.thread_count(5, true)<<" io threads"<<std::endl;
+  std::cout<<"Main is "<<boost::this_thread::get_id()<<std::endl;
   
-  merged_signal::signal_type a(io.service()), b(io.service());
+  clk.start(12);
   
-  a.connect(boost::bind(tick, "A", _1));
-  b.connect(boost::bind(tick, "B", _1));
-  
-  reactor.connect_to(a);
-  reactor.connect_to(b);
-  
-  reactor.connect(boost::bind(&tick_incr, boost::ref(a), _1));
-  a.connect(boost::bind(&tick_incr, boost::ref(b), _1));
-  
-  io.thread_count(10);
-  
-  a(0);
-  a(1);
-  a(2);
-  b(0);
-  b(1);
-  a(3);
-  b(0); // A glitch for testing
-  a(1); // second glitch that should mess me up
-  b(2);
-  b(3);
-  a(4);
-  b(4);
-  
+#if 0
   sleep(10);
-  io.service().stop();
+  clk.stop();
+#else
+  clk.wait_completion();
+#endif
+  
   
   return 0;
 }
-
-
