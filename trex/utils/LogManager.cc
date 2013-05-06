e7a4ba8a0087b5580ef67cc375168116e6f08b41
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-05-06 19:22:29 +0000

    [core] commented the reason why log_pipe is disabled

--- trex/utils/LogManager.cc
+++ trex/utils/LogManager.cc
@@ -60,10 +60,13 @@ using namespace boost::filesystem;
 // structors :
 
 LogManager::LogManager():m_inited(false), m_syslog(m_io.service()), m_level(TREX_LOG_LEVEL) {
+  // Disabled following code as it is not thread safe
+  /*
   // Capture standard output and error in TREX.log
-//  m_out.reset(new log::log_pipe(m_syslog, std::cout, "_cout_", ""));
-//  m_log.reset(new log::log_pipe(m_syslog, std::clog, "_clog_", ""));
-//  m_err.reset(new log::log_pipe(m_syslog, std::cerr, "_cerr_", "ERROR"));
+  m_out.reset(new log::log_pipe(m_syslog, std::cout, "_cout_", ""));
+  m_log.reset(new log::log_pipe(m_syslog, std::clog, "_clog_", ""));
+  m_err.reset(new log::log_pipe(m_syslog, std::cerr, "_cerr_", "ERROR"));
+`*/
 }
 
 LogManager::~LogManager() {
