c00f2f9d1e6b7d85d30a3881fcce658489cc3d21
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-06-06 18:12:53 +0000

    [rest] Properly handled the request for an unknown timeline by returning an error instead of waiting indefinitely

--- trunk/extra/REST/TimelineHistory.cc
+++ trunk/extra/REST/TimelineHistory.cc
@@ -339,7 +339,9 @@ size_t TimelineHistory::get_tok_sync(TREX::utils::Symbol tl,
                                      bool first, size_t max) {
   size_t ret = 0;
   helpers::rest_tl_set::const_iterator pos = m_timelines.find(tl);
-  if( m_timelines.end()!=pos ) {
+  if( m_timelines.end()==pos )
+    throw std::runtime_error("Unknown timeline "+tl.str());
+  else {
     if( !first )
       out.put(',');
     if( (*pos)->has_observation() ) {
