f96ce37e2e85e61ea5aa8781a5eb152396db126d
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-07-30 21:34:43 +0000

    [utils] removed dangling uses of boost:::shared_ptr instead of SHARED_PTR macro

--- trunk/trex/transaction/bits/transaction_fwd.hh
+++ trunk/trex/transaction/bits/transaction_fwd.hh
@@ -36,8 +36,6 @@
 
 # include <trex/utils/LogManager.hh>
 
-# include <boost/shared_ptr.hpp>
-
 # include <bitset>
 
 namespace TREX {
@@ -47,7 +45,7 @@ namespace TREX {
       class graph_impl;
       
       class node_impl;
-      typedef boost::weak_ptr<node_impl> node_id;
+      typedef WEAK_PTR<node_impl> node_id;
       
       /** @brief Reactor transaction flags
        *
