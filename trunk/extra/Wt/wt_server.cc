6a6a6e4dac4366ba147a231b8e9e76038fc20a0b
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-10-02 00:24:42 +0000

    [extra] started a global Wt interface library with specifically the http server being presented as a singleton [extra/REST] modified rest plugins to use the library above and handle server through the given singleton (still need to be done in witre)

--- trunk/extra/Wt/wt_server.cc
+++ trunk/extra/Wt/wt_server.cc
@@ -1,13 +1,14 @@
+/* -*- C++ -*- */
 /*********************************************************************
  * Software License Agreement (BSD License)
- * 
- *  Copyright (c) 2011, MBARI.
+ *
+ *  Copyright (c) 2013, MBARI.
  *  All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
- * 
+ *
  *   * Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
@@ -17,7 +18,7 @@
  *   * Neither the name of the TREX Project nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -31,47 +32,52 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef H_trex_REST_reactor
-# define H_trex_REST_reactor
+#include <trex/wt/server.hh>
 
-# include <trex/transaction/TeleoReactor.hh>
-# include <trex/Wt/server.hh>
+using namespace TREX::wt;
 
-# include <boost/signals2/signal.hpp>
+/*
+ * class TREX::Wt::server
+ */
 
-namespace TREX {
-  namespace REST {
-    
-    class service_tree;
-    class TimelineHistory;
-    class tick_manager;
-    
-    class REST_reactor :public transaction::TeleoReactor {
-    public:
-      REST_reactor(TREX::transaction::TeleoReactor::xml_arg_type arg);
-      ~REST_reactor();
-      
-    private:
-      //reactor handlers
-      void handleInit();
-      void handleTickStart();
-      void notify(TREX::transaction::Observation const &obs);
-      bool synchronize();
-      void newPlanToken(transaction::goal_id const &t);
-      void cancelledPlanToken(transaction::goal_id const &t);
-      
-      TREX::utils::SingletonUse<TREX::wt::server> m_server;
-      
-      // UNIQ_PTR<Wt::WServer>     m_server;
-      
-      SHARED_PTR<TimelineHistory> m_timelines;
-      SHARED_PTR<tick_manager>    m_tick;
-      UNIQ_PTR<service_tree>             m_services;
-      
-      friend class TimelineHistory;
-    };
-    
-  }
+// structors
+
+server::server() {}
+
+server::~server() {
+  m_server.reset();
 }
 
-#endif // H_trex_REST_reactor
+// observer
+
+bool server::is_inited() const {
+  return NULL!=m_server.get();
+}
+
+bool server::is_running() const {
+  return is_inited() && m_server->isRunning();
+}
+
+::Wt::WServer &server::impl() const {
+  return *m_server;
+}
+
+// modifiers
+
+void server::init(size_t argc, char *argv[], boost::optional<std::string> const &cfg) {
+  if( is_inited() )
+    throw std::runtime_error("server already inited");
+  if( cfg )
+    m_server.reset(new ::Wt::WServer("", *cfg));
+  else
+    m_server.reset(new ::Wt::WServer(""));
+  m_server->setServerConfiguration(argc, argv);
+}
+
+bool server::start() {
+  if( is_inited() ) {
+    if( m_server->isRunning() )
+      return m_server->start();
+  }
+  return false;
+}
