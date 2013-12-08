ffe3d2c6e8c6e09dd7aad56fdfc588f68f75c52d
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2013-12-08 17:30:52 +0000

    Final -- really this time -- merge corresponding to the version provided as a package in the project page

---
+++ extra/third_party/ros/trex/ros/ros_convert_traits.hh
@@ -0,0 +1,16 @@
+#ifndef H_trex_ros_ros_convert_traits 
+# define H_trex_ros_ros_convert_traits 
+
+# include <ros/message_traits.h>
+
+namespace TREX {
+  namespace ROS {
+
+    template<typename Message>
+    struct ros_convert_traits;
+
+  }
+}
+
+
+#endif // H_trex_ros_ros_convert_traits
