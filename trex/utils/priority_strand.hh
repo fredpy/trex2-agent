9835e3dad2b73f4e535f0b73e15d96a7684d5c2e
fredpy@gmail.com <fredpy@gmail.com@eff219cc-a5a7-32d8-cb09-f88e880e2402>
2014-06-06 17:33:37 +0000

    [utils/priority_strand]   - Added the ability to send an action along with a simpleton handler   - reimplemented utility classes for the post() methods to rely on this new construct   NOTE: the send method conflict between themselves in some case. I need to improve this API so the compiler do not get confused between send(&fn, &handler) and send(&fn, 0)

--- trex/utils/priority_strand.hh
+++ trex/utils/priority_strand.hh
@@ -39,8 +39,10 @@
 # include <boost/asio/strand.hpp>
 # include <boost/optional.hpp>
 # include <boost/thread/future.hpp>
+# include <boost/thread/mutex.hpp>
 # include <boost/thread/shared_mutex.hpp>
 # include <boost/utility/result_of.hpp>
+# include <boost/tuple/tuple.hpp>
 
 # include "platform/system_error.hh"
 
@@ -48,13 +50,185 @@
 
 namespace TREX {
   namespace utils {
-     
+    
+    namespace details {
+      
+      template<typename Ret>
+      class async_task;
+      
+    } // TREX::utils::details
+   
+    
+#ifndef DOXYGEN
+    
+    template<typename Ret>
+    class async_result;
+    
+  
+    template<>
+    class async_result<void> {
+    public:
+      ~async_result() {}
+      
+      bool is_exception() const {
+        return bool(m_error);
+      }
+      boost::exception_ptr const &exception() const {
+        return m_error;
+      }
+      void get() const {
+        if( m_error )
+          rethrow_exception(m_error);
+      }
+      void get(boost::promise<void> &p) const {
+        if( m_error )
+          p.set_exception(m_error);
+        else
+          p.set_value();
+      }
+
+    private:
+      async_result() {}
+      
+      void set_exception(boost::exception_ptr const &e) {
+        m_error = e;
+      }
+      
+      void set(boost::function<void ()> const &fn) {
+        try {
+          fn();
+        } catch(...) {
+          set_exception(boost::current_exception());
+        }
+      }
+      
+      boost::exception_ptr m_error;
+      
+      friend details::async_task<void>;
+      
+      template<typename Ret>
+      friend class async_result;
+    }; // TREX::utils::async_result<void>
+    
+    
+    template<typename Ret>
+    class async_result {
+    public:
+      ~async_result() {}
+      
+      bool is_exception() const {
+        return m_except.is_exception();
+      }
+      boost::exception_ptr const &exception() const {
+        return m_except.exception();
+      }
+      Ret const &get() const {
+        m_except.get();
+        return *m_value;
+      }
+      void get(boost::promise<Ret> &p) const {
+        if( is_exception() )
+          p.set_exception(exception());
+        else
+          p.set_value(*m_value);
+      }
+      
+    private:
+      async_result() {}
+      
+      void set(boost::function<Ret ()> const &fn) {
+        m_except.set(boost::bind(&async_result::set_value, this,
+                                 boost::bind(fn)));
+      }
+      void set_value(Ret const &v) {
+        m_value = v;
+      }
+      void set_exception(boost::exception_ptr const &e) {
+        m_except.set_exception(e);
+      }
+      
+      
+      async_result<void>   m_except;
+      boost::optional<Ret> m_value;
+      
+      friend details::async_task<Ret>;
+    }; // TREX::utils::async_result<>
+    
+# else // DOXYGEN
+    
+    /** @brief Asynchronous task result placeholder
+     *
+     * This class is used by @c priority_strand to pass the 
+     * result of the asynchronously executed task to a potential 
+     * handling callback.
+     *
+     * The class itself present aninterface largely inspired from
+     * boost::future or std::future from C++11. It can embed either
+     * the retruned value from the asynchronous call or the exception 
+     * that was produced by this call.
+     *
+     * @tparam Ty the return type of the function
+     *
+     * @relates priority_strand
+     */
+    template<typename Ty>
+    class async_result {
+    public:
+      /** @brief Check if exception
+       *
+       * Check if this result is an excpetion
+       *
+       * @retval true if the result is an exception
+       * @retval false otherwise
+       */
+      bool is_exception() const;
+      /** @brief Get exception 
+       *
+       * If this instance holds an exception it returns a 
+       * pointer to this exception or an empty pointer 
+       * otherwise.
+       *
+       * @return A pointer to the exception if any
+       * @sa is_exception() const
+       */
+      boost::exception_ptr const &exception() const;
+      /** @brief Get outcome of the function
+       *
+       * This function reporduces the outcome of the
+       * asynchronous fucntion. If the function threw 
+       * an exception then this call will rethrow it 
+       * otherwise it returns the value of the function 
+       * if any.
+       *
+       * @note computed_type is convertible into Ty
+       *
+       * @throw an exception if is_excpetion() is true
+       *
+       * @return The returned value of the function
+       * @sa is_exception() const
+       */
+      computed_type get() const;
+      /** @brief transfert the value to a boost::promise
+       *
+       * @param[out] promise the promise to update
+       *
+       * Updates @p promise with the value or exception 
+       * wrapped by this instance.
+       */
+      void get(boost::promise<Ty> &promise) const;
+    };
+    
+# endif // DOXYGEN
+    
+    
     namespace details {
       
       template<typename Fn>
       struct task_helper {
         typedef typename boost::result_of<Fn()>::type return_type;
         typedef boost::shared_future<return_type>     future;
+        
+        typedef async_result<return_type>             wrapper;
       }; // TREX::utils::details::task_helper<>
       
     } // TREX::utils::details
@@ -79,7 +253,6 @@ namespace TREX {
      */
     class priority_strand {
     public:
-      
       class task {
       public:
         typedef size_t priority;
@@ -109,7 +282,13 @@ namespace TREX {
        * managed by @p io
        */
       explicit priority_strand(boost::asio::io_service &io);
-      /** @brief Detructor */
+      /** @brief Detructor 
+       *
+       * clear the queue of pending tasks for this instance and 
+       * destroy this instance.
+       *
+       * @dsa clear()
+       */
       ~priority_strand();
       
       /** @{
@@ -142,28 +321,33 @@ namespace TREX {
        *
        * @tparam Fn A functor or function type
        *
-       * @param[in] f The task to execute
-       * @param[in] p A priority value
+       * @param[in] f      The task to execute
+       * @param[in] handle A function to call on completion
+       * @param[in] p      A priority value
        *
        * Schedule @p f to be executed with the priority @p p. If @p p
        * is not provided then the tasl priority is set to the lowest
-       * priority. The highest priority is 0 follewed by 1, ...
+       * priority. The highest priority is 0 followed by 1, ...
        *
        * This call return immediately with the task @p f being scheduled
-       * for execution. 
+       * for execution. If @p handle is provided it will be called after 
+       * @p f is executed with its @c async_result as an argument.
        *
-       * @note This method do not give any way to synchronize directly 
-       * with the completion of @p f. If such synchronization is needed 
-       * please uses @c priority_starnd::post instead
        *
        * @pre Fn is a functor with no argument (ie @p f() is valid)
+       * @pre Handle is a functor of the form @c void(async_result<Ret> const &).
+       * Where @c Ret is return type of @p Fn
        *
        * @sa priority_strand::post
        */
       template<typename Fn>
       void send(Fn f);
+      template<typename Fn, typename Handler>
+      void send(Fn f, Handler handle);
       template<typename Fn>
       void send(Fn f, priority_type p);
+      template<typename Fn, typename Handler>
+      void send(Fn f, Handler handle, priority_type p);
       /** @} */
       
       /** @brief Number of pending tasks
@@ -184,6 +368,13 @@ namespace TREX {
        */
       bool empty() const;
       
+      /** @brief remove all pending tasks 
+       *
+       * Clear all the tasks that were either posted or sent but 
+       * not yet executed. The result of all these tasks is set 
+       * to a @c SYSTEM_ERROR exception with the error code
+       * @c ERRC::operation_canceled
+       */
       void clear();
       
     private:
