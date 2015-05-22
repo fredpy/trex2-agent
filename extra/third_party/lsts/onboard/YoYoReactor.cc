c37ca947289f4d95e098a30606f44d4da5a91f62
José Pinto <zepinto@gmail.com>
2015-05-22 21:25:05 +0100

    YoYoReactor: Fixed calculation.

--- extra/third_party/lsts/onboard/YoYoReactor.cc
+++ extra/third_party/lsts/onboard/YoYoReactor.cc
@@ -135,7 +135,7 @@ namespace TREX {
         double cur_depth = depth.domain().getTypedSingleton<double,0>();
 
         dist_to_target = WGS84::distance(cur_lat, cur_lon, 0, m_lat, m_lon, 0);
-        nearEnd = dist_to_target < (cur_depth / std::sin(m_pitch)) * m_speed;
+        nearEnd = dist_to_target < (cur_depth / std::sin(m_pitch));
       }
 
       double atZ = -1000;
